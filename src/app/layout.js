345200ce8bd9593a39ce5e72e2261cc2f484c705
Shafi-Anwar <sufiyananwar559@gmail.com>
2024-09-07 16:51:07 +0530

    dw

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
 import { Providers } from "../providers";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,7 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>{children}
+
+          <Analytics />
+        </Providers>
       </body>
     </html>
   );
